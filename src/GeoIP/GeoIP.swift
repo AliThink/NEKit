d52493aa0abfc90d89a4f1ef0718d86d50d723a3
AliThink <alithink@outlook.com>
2020-01-06 11:54:13 +0800

    更新geo

--- src/GeoIP/GeoIP.swift
+++ src/GeoIP/GeoIP.swift
@@ -2,7 +2,7 @@ import Foundation
 import MMDB
 
 open class GeoIP {
-    public static let database = MMDB("nekitdb")!
+    public static let database = MMDB(Bundle().path(forResource: "GeoLite2-Country", ofType: "mmdb")!)!
 
     public static func LookUp(_ ipAddress: String) -> MMDBCountry? {
         return GeoIP.database.lookup(ipAddress)
